22fc111d161a2785ccc2dfe86dbb09ddec4f77ad
soongu <naong526@naver.com>
2025-08-29 14:30:13 +0900

    14. 카테고리 클릭 이벤트

--- src/components/BlogSideBar.jsx
+++ src/components/BlogSideBar.jsx
@@ -1,7 +1,23 @@
 import styles from './BlogSideBar.module.scss';
 import { categories } from '../dummy-data/dummy-post.js';
+import {useNavigate, useSearchParams} from 'react-router-dom';
 
 const BlogSideBar = () => {
+
+  const [, setSearchParams] = useSearchParams();
+
+  // 새로고침 없이 리다이렉션을 지원하는 훅
+  const navigate = useNavigate();
+
+  const handleCategoryClick = (category) => {
+    // 사이드바에 있는 카테고리를 누르면 목록으로 이동하면서 카테고리를 보여줘야 함.
+    // location.href = '/blog';
+    navigate(`/blog`);
+    setSearchParams(prev => {
+      prev.set('category', category);
+      return prev;
+    });
+  };
 
   return (
     <aside className={styles.sidebar}>
@@ -11,6 +27,7 @@ const BlogSideBar = () => {
           <li key={category.id}>
             <button
               className={`${styles.categoryButton}`}
+              onClick={(e) => handleCategoryClick(category.id)}
             >
               {category.name}
               <span className={styles.count}>
